ed48bae9462a083f25ec53ef8da218fcaa87736f
Oscar Molina <oscar.molina3328@gmail.com>
2023-01-26 14:17:20 -0600

    multiplication update

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,29 @@
 
-const number = parseInt(prompt('Enter an integer: '));
+// const number = parseInt(prompt('Enter an integer: '));
+//
+// for(var i = 1; i <= 10; i++) {
+//
+//     const result = i * number;
+//
+//     console.log(number * i);
+// }
 
-for(let i = 1; i <= 10; i++) {
+// 123 is odd
+// 80 is even
+// 24 is even
+// 199 is odd
+// ...
 
-    const result = i * number;
 
-    console.log(number * i);
+function oddNumber(input){
+    console.log("This number should be skipped" + input);
+    for (let i = 0; i < 50; i++){
+        if ( i % 2 === 0){
+        }
+        else if ( i !== input){
+            console.log("Here is an odd number!:" + 1);
+        } else {
+            console.log("Ewww skip that number :" + 1);
+        }
+    }
 }
