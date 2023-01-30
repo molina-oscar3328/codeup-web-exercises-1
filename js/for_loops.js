c379a8426601a7931b14251af527c41ab19e04ab
Oscar Molina <oscar.molina3328@gmail.com>
2023-01-30 15:22:15 -0600

    update for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,6 +8,37 @@
 //     console.log(number * i);
 // }
 
+/* program to generate a multiplication table
+upto a range */
+
+// take number input from the user
+// let number = parseInt(prompt('Enter an integer: '));
+//
+// // take range input from the user
+// let range = parseInt(prompt('Enter a range: '));
+//
+// //creating a multiplication table
+// for(let i = 1; i <= range; i++) {
+//     let result = i * number;
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+
+
+// let number = parseInt(prompt("Hello can you pick a number please"));
+// let range = parseInt(prompt("How many times would you like to multiply this by?"));
+//
+// for(let i = 1; i <= range; i++){
+//     let result = i * number;
+//     console.log (`${number} * ${i} = ${result}`);
+// }
+
+// for(let i = 1; 1 <= 10; i++){
+//     let result = i * 1;
+//     console.log (`${1} * ${i} = ${result}`);
+// }
+
+
 // 123 is odd
 // 80 is even
 // 24 is even
@@ -27,6 +58,9 @@
 //         }
 //     }
 // }
+
+
+
 
 
 
