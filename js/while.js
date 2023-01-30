412e54edb304a8ab356dad7976d39bfcbad88be3
Oscar Molina <oscar.molina3328@gmail.com>
2023-01-30 16:31:42 -0600

    while loop added

--- js/while.js
+++ js/while.js
@@ -9,14 +9,14 @@ while(i <= 65536){
 var random = Math.floor((Math.random() * 50) +50)
 do {
     var conesSold = Math.floor((Math.random()* 4) + 1);
-    if(conesSold < cones){
+    if(conesSold < cones) {
         console.log(conesSold + " cones sold....");
-        cones =- conesSold;
+        cones -= conesSold;
     } else if (conesSold > cones){
         console.log("cannot sell you " + conesSold + "cones I only have " + cones);
     }else {
         console.log("I sold them all!");
-        cones +- conesSold;
+        cones -= conesSold;
 
     }
 } while(cones > 0)
