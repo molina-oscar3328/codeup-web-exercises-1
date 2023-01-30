23a5bbfe06e8211a3f94d71614cc666425ed4078
Oscar Molina <oscar.molina3328@gmail.com>
2023-01-30 16:13:46 -0600

    update 2

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -8,14 +8,35 @@
 // console.log("Good afternoon!");}
 // else
 // console.log("Good evening");
-let myAge = 20;
-prompt("Hello how old are you?")
-if (myAge >= 15 && myAge < 25){
-    alert ("You are very young and strong aren't you?");
+// let myAge = 20;
+// prompt("Hello how old are you?")
+// if (myAge >= 15 && myAge < 25){
+//     alert ("You are very young and strong aren't you?");
+// }
+// else if (myAge >= 25 && myAge < 30){
+//     alert("Hey does your back hurt yet chief?")
+// }
+
+
+
+var userInput = "";
+while(true){
+   userInput = Number(prompt("enter an odd number between 1 and 50 chief"));
+   if(userInput % 2 === 1 && userInput >= 1 && userInput <= 50){
+       break;
+   }
 }
-else if (myAge >= 25 && myAge < 30){
-    alert("Hey does your back hurt yet chief?")
+
+for(var i = 1; 1 <= 50; i += 2){
+    console.log("here is the odd number")
 }
+
+
+
+
+
+
+
 
 
 
