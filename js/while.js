ca3b0c858e1b1e8b094d880fb693f8528870defe
Oscar Molina <oscar.molina3328@gmail.com>
2023-01-30 16:25:49 -0600

    while loop update

---
+++ js/while.js
@@ -0,0 +1,22 @@
+"use strict"
+
+var i = 2;
+while(i <= 65536){
+    console.log(i);
+    i = i * 2;
+}
+
+var random = Math.floor((Math.random() * 50) +50)
+do {
+    var conesSold = Math.floor((Math.random()* 4) + 1);
+    if(conesSold < cones){
+        console.log(conesSold + " cones sold....");
+        cones =- conesSold;
+    } else if (conesSold > cones){
+        console.log("cannot sell you " + conesSold + "cones I only have " + cones);
+    }else {
+        console.log("I sold them all!");
+        cones +- conesSold;
+
+    }
+} while(cones > 0)
