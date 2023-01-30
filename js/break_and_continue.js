f7c23d2596ad18b2add871ae0c45fd6a3482277f
Oscar Molina <oscar.molina3328@gmail.com>
2023-01-29 22:36:59 -0600

    update

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,21 @@
-prompt("Pick a number between 1 and 50")
+// prompt("Pick an odd number between 1 and 50")
+
+
+// let hour = 10;
+// if (hour >= 6 && hour < 12) {
+//     console.log("Good morning");}
+// else if (hour >= 12 && hour < 18){
+// console.log("Good afternoon!");}
+// else
+// console.log("Good evening");
+let myAge = 20;
+prompt("Hello how old are you?")
+if (myAge >= 15 && myAge < 25){
+    alert ("You are very young and strong aren't you?");
+}
+else if (myAge >= 25 && myAge < 30){
+    alert("Hey does your back hurt yet chief?")
+}
+
+
+
