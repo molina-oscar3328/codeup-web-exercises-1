4d0273a45bb17cebd823f4eb55e68ab2108bb1c3
Oscar Molina <oscar.molina3328@gmail.com>
2023-01-31 22:26:01 -0600

    update 4

--- js/iterating.js
+++ js/iterating.js
@@ -1,100 +1,129 @@
-"use strict";
-
-/**
- * TODO:
- * Create an array of 4 people's names and store it in a variable called
- * 'names'.
- */
-
-var names = ["bertha", "luis", "erick","sam"];{
-    console.log(names);
-}
-
-
-/**
- * TODO:
- * Create a log statement that will log the number of elements in the names
- * array.
- */
-
-var names = ["bertha", "luis", "erick","sam"];{
-    console.log(names.length);
-}
-
-
-/**
- * TODO:
- * Create log statements that will print each of the names individually by
- * accessing each element's index.
- */
-
-var names = ["bertha", "luis", "erick","sam"];
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
-
-
-
-
-
-
-
-/**
- * TODO:
- * Write some code that uses a for loop to log every item in the names
- * array.
- */
-
-var names = ["bertha", "luis", "erick","sam"];
-
-for (var i = 0; i < names.length; i++) {
-    // console.log('The names at index ' + i + ' is: ' + names[i]);
-    console.log(`The names at index ${i} is: ${names[i]}`);
-}
-
-
-/**
- * TODO:
- * Refactor your above code to use a `forEach` loop
- */
-var names = ["bertha", "luis", "erick","sam"];
+// "use strict";
+//
+// /**
+//  * TODO:
+//  * Create an array of 4 people's names and store it in a variable called
+//  * 'names'.
+//  */
+//
+// var names = ["bertha", "luis", "erick","sam"];{
+//     console.log(names);
+// }
+//
+//
+// /**
+//  * TODO:
+//  * Create a log statement that will log the number of elements in the names
+//  * array.
+//  */
+//
+// var names = ["bertha", "luis", "erick","sam"];{
+//     console.log(names.length);
+// }
+//
+//
+// /**
+//  * TODO:
+//  * Create log statements that will print each of the names individually by
+//  * accessing each element's index.
+//  */
+//
+// var names = ["bertha", "luis", "erick","sam"];
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+//
+//
+//
+//
+//
+//
+//
+// /**
+//  * TODO:
+//  * Write some code that uses a for loop to log every item in the names
+//  * array.
+//  */
+//
+// var names = ["bertha", "luis", "erick","sam"];
+//
+// for (var i = 0; i < names.length; i++) {
+//     // console.log('The names at index ' + i + ' is: ' + names[i]);
+//     console.log(`The names at index ${i} is: ${names[i]}`);
+// }
+//
+//
+// /**
+//  * TODO:
+//  * Refactor your above code to use a `forEach` loop
+//  */
+// var names = ["bertha", "luis", "erick","sam"];
 // names.forEach(function(name){
 //     console.log('Here is a lovely name: ' + name + '.');});
-    // console.log(`Here is a nice name ${name}.`);});
-
-
-
-
-
-
-/**
- * TODO:
- * Create the following three functions, each will accept an array and
- * return an an element from it
- * - first: returns the first item in the array
- * - second: returns the second item in the array
- * - last: returns the last item in the array
- *
- * Example:
- *  > first([1, 2, 3, 4, 5]) // returns 1
- *  > second([1, 2, 3, 4, 5]) // returns 2
- *  > last([1, 2, 3, 4, 5]) // return 5
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//     // console.log(`Here is a nice name ${name}.`);});
+//
+//
+//
+//
+//
+//
+// /**
+//  * TODO:
+//  * Create the following three functions, each will accept an array and
+//  * return an an element from it
+//  * - first: returns the first item in the array
+//  * - second: returns the second item in the array
+//  * - last: returns the last item in the array
+//  *
+//  * Example:
+//  *  > first([1, 2, 3, 4, 5]) // returns 1
+//  *  > second([1, 2, 3, 4, 5]) // returns 2
+//  *  > last([1, 2, 3, 4, 5]) // return 5
+//  */
+//
+// // var todoList = ['Go to the bank', 'Clean the car', 'Buy a dog'];
+// //
+// // // console.log('My todo list:');
+// // console.log(todoList);
+// // // console.log('Completing the last item: ' + todoList[todoList.length - 1]);
+// // console.log(`Last item complete ${todoList[todoList.length - 1]}`);
+// // var removedItem = todoList.pop();
+// // console.log(`Task complete ${removedItem}`);
+// // console.log(todoList);
+// // console.log(`Complete the first item ${todoList[1]}`);
+// // var doneItem = todoList.shift();
+// // console.log(`Task has been completed at this time ${doneItem}`);
+// // console.log(todoList);
+//
+//
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black'];
+//
+// var index = colors.indexOf('green');
+// console.log(index);
+//
+// index = colors.indexOf('red');
+// console.log(index);
+//
+// index = colors.lastIndexOf('red');
+// console.log(index);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 
