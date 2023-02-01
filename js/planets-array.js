c3c20af3e0515a979a93a45acc44c33ef5184429
Oscar Molina <oscar.molina3328@gmail.com>
2023-02-01 12:27:37 -0600

    lesson array update

---
+++ js/planets-array.js
@@ -0,0 +1,79 @@
+// var num = "hello"
+// console.log(num);
+//
+//
+// var number = ['1', '2', '3', '4'];
+// console.log(number);
+// number.push('5');
+// console.log(number);
+// number.push('6', '7', '8');
+// console.log(number);
+// number.unshift('33', '22', '11', '00');
+// console.log(number);
+// console.log("______next example________"); //<-------------
+//
+//
+// var number = ['1', '2', '3', '4'];
+//
+// number.shift();
+// console.log(number);
+// number.pop();
+// console.log(number);
+// console.log("______reverse examples________"); //<-------------
+//
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+//
+// console.log(colors);
+//
+// colors.reverse();
+//
+// console.log(colors);
+//
+// console.log("________sorting_______"); //<--------------
+//
+// console.log(colors);
+//
+// colors.sort();
+//
+// console.log(colors);
+//
+// console.log("_______splitting method________"); //<-------------
+//
+// var months = "jan,feb,mar,apr,may,jun,jul,aug"
+// var arrayMonths = months.split(",");
+// console.log(arrayMonths);
+//
+// var hornet = "Hornet the great";
+// var hornetArray = hornet.split(" ");
+// console.log(hornetArray);
+//
+// var sentence = "hello how are you doing?"
+// var emptySplit = sentence.split();
+// console.log(emptySplit);
+//
+// console.log("__________joining example_____________") // <-----------
+//
+// var elements = ['fire', 'air', 'water'];
+// console.log(elements.join(","));
+// console.log(elements.join(" + "));
+// console.log(elements.join(" ? "));
+// console.log(elements.join(" / "));
+// console.log(elements.join(" ---- "));
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
