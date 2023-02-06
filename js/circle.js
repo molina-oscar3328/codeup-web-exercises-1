c962960056bd883a04f2259b33ea0d561c194b8e
Oscar Molina <oscar.molina3328@gmail.com>
2023-02-06 09:11:03 -0600

    update

---
+++ js/circle.js
@@ -0,0 +1,71 @@
+(function() {
+    "use strict";
+
+    // // create a circle object
+    // var circle = {
+    //     radius: 3,
+    //
+    //     getArea: function () {
+    //         // TODO: complete this method
+    //         // hint: area = pi * radius^2
+    //
+    //         return; // TODO: return the proper value
+    //     },
+    //
+    //     logInfo: function (doRounding) {
+    //         // TODO: complete this method.
+    //
+    //         // If doRounding is true, round the result to the nearest integer.
+    //         // Otherwise, output the complete value
+    //
+    //         console.log("Area of a circle with radius: " + this.radius + ", is: ");
+    //     }
+    // };
+    //
+    // // log info about the circle
+    // console.log("Raw circle information");
+    // circle.logInfo(false);
+    // console.log("Circle information rounded to the nearest whole number");
+    // circle.logInfo(true);
+    //
+    // console.log("=======================================================");
+    // // TODO: Change the radius of the circle to 5.
+    //
+    // // log info about the circle
+    // console.log("Raw circle information");
+    // circle.logInfo(false);
+    // console.log("Circle information rounded to the nearest whole number");
+    // circle.logInfo(true);
+
+
+
+// var x = 1;
+// x++;
+// console.log(x);
+//     console.log(/-------NEXT---------/);
+//
+
+//  if(number === 1) {
+//      console.log("one");
+//  } else if (number === 2){
+//      console.log("two");
+//      } else if(number === 3){
+//          console.log("three");
+//      }else(
+//          console.log("not the number fam look somewhere else")
+//      )
+
+    // var pizzaPreference = prompt("What kind of pizza do you like?");
+    //
+    // if (pizzaPreference === "pineapple and hot sauce") {
+    //     alert("What a coincidence, that's my favorite!");
+    // } else if (pizzaPreference === "cheese") {
+    //     alert("Just plain cheese? Okay...");
+    // } else {
+    //     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+    // }
+
+
+
+
+})();
