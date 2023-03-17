a03122557fcf91fea854acdfa200669ea55fe8b9
Oscar Molina <oscar.molina3328@gmail.com>
2023-03-17 16:33:55 -0500

    example deleted

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,17 +36,22 @@ const users = [
     }
 
     ];
+// TODO: Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 
-//     let newUsers = [];
-//     let userLanguages = users.filter(function(user){
-//       if(user.languages.length >= 3){
-//           newUsers.push(users);
-//       }
-//   })
-// console.log(newUsers);
+//     let threeLanguages = users.filter(function(language){
+//         return users.language.length >= 3
+//     })
+// console.log(threeLanguages);
 
-    let usersEmail = users.map(function(email){
-        return users + email
-})
-console.log(usersEmail);
+// TODO: Use .map to create an array of strings where each element is a user's email address
+
+    let usersEmail = users.map (users => users.email)
+    console.log(usersEmail);
+
+    // TODO: Use .reduce to get the longest email from the list of users.
+
+    let longEmail = users.reduce((longest, user) => {
+        return user.email.length > longest.length ? user.email : longest} , ' ');
+    console.log(longEmail);
+
 
